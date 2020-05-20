c7129ac767daa6ecf032125f6c31f743bee2adb6
cute1baby <1138724604@qq.com>
2020-05-20 23:49:22 +0800

    增加文件

--- 3miaovKoa/24pro/front_back/app/index.ts
+++ 3miaovKoa/24pro/front_back/app/index.ts
@@ -1,11 +1,13 @@
 // const Koa = require('koa')  这种是commonjs的用法，现在要用es6的写法
 
 import * as Koa from 'koa'
-
+import { useControllers } from 'koa-controllers'
 let app = new Koa()
-
-app.use(async ctx => {
-    ctx.body = 'hello，我是小李'
+// 引入控制器文件
+useControllers(app, __dirname + '/controllers/*.js', {
+    multipart: {
+        dest: './uploads'
+    }
 })
 
 app.listen(8080)
