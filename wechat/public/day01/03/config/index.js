95f3d8c2ba2adc9ab96b7afb20440b508a37b995
cute1baby <1138724604@qq.com>
2020-08-19 00:57:52 +0800

    access_token相关的数据操作

---
+++ wechat/public/day01/03/config/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    token: 'laoli0629shezhimogen10011',
+    appID: 'wxd9f5805ea8e18784',
+    appsecret: '39501d5d4b78eca0c1f1593481c76165'
+}
---
+++ wechat/public/day01/readme.md
@@ -0,0 +1,6 @@
+### 以下是对项目的介绍
+- 01 验证服务器的有效性，一个文件版
+- 02 验证服务器的有效性，文件模块化
+- 03 获取access_token(安装两个库)
+    - request
+    - request-promise-native
