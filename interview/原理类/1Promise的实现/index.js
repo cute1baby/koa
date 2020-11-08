3209808e738932e2a97ec63b766bcf46d262f8f1
cute1baby <1138724604@qq.com>
2020-11-09 00:19:55 +0800

    值穿透的例子

--- interview/原理类/1Promise的实现/index.js
+++ interview/原理类/1Promise的实现/index.js
@@ -14,3 +14,14 @@ new Promise((resolve, reject) => {
     console.log('reason', reason)
 })
 console.log('3')
+
+
+
+new Promise((resolve, reject) => {
+    resolve(1)
+})
+// 值穿透的问题
+.then(10)
+.then(res => {
+    console.log(res)
+})
