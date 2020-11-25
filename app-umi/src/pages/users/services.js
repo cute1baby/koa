0ea2fdb11a65d5319a68dba8e5dcecb9eaf11b92
cute1baby <1138724604@qq.com>
2020-11-25 23:46:24 +0800

    数据提交

--- app-umi/src/pages/users/services.js
+++ app-umi/src/pages/users/services.js
@@ -1,7 +1,7 @@
 import request from '@/utils/request';
 
 export async function getQuestionList(params) {
-    return request("http://62.234.192.179:7300/mock/5fbe09e86bfbdb50beec3ab0/umi/getTableList", {
+    return request("http://62.234.192.179:7300/mock/5fbe747546d1046d9c86d11b/umi/getTableList", {
         method: "POST",
         body: params
     })
