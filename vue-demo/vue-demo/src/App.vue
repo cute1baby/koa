11f92263a7d67c6cf5b3888ce06ea1d44a4dcc27
cute1baby <1138724604@qq.com>
2020-12-17 22:52:00 +0800

    提交笔记

---
+++ vue-demo/vue-demo/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    {{message}}
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+//   data(){
+//       return {
+//         message: 'hello vue'
+//       }
+//   }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
