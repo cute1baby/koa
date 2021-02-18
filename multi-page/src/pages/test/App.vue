ea2fea1c2bc8c18488db10dc3d61f7cae69af6c2
cute1baby <1138724604@qq.com>
2021-02-18 11:50:20 +0800

    修改多页应用模板

---
+++ multi-page/src/pages/test/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    我是测试页面
+    <br>
+    <p>试一下双向数据绑定：{{msg}}</p>
+    <hr>
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+export default {
+  name: 'App',
+  data () {
+    return {
+      msg: '下面是组件'
+    }
+  },
+  components: {
+      HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
